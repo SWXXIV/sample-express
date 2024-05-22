cc0b8f80e55867223353133c1f2f11c3771e2515
Nick Kelly <k05nk01@gmail.com>
2024-05-21 17:37:06 -0700

    Adding react to express app

--- index.ts
+++ index.ts
@@ -1,24 +1,46 @@
-import { routeHello, routeAPINames } from './routes.js';
-import express from "express";
+import { routeHello, routeAPINames, routeWeather } from './routes.js';
+import express, { Request, Response } from "express";
+
+import path from "path";
 
 const server = express();
 const port = 3000;
 
-server.get('/hello', function (req, res) {
-    const response = routeHello(req, res);
-    res.send(response);
-});
-
-server.get("/api/names", async function (req, res) {
-    let response;
-    try {
-        response = await routeAPINames(req, res);
-    } catch (err) {
-        console.log(err);
+server.get(
+    "/hello",
+    function (_req: Request, res: Response): void {
+        const response = routeHello();
+        res.send(response);
     }
-    res.send(response);
+);
+
+server.get(
+    "/api/names", 
+    async function (_req: Request, res: Response): Promise<void> {
+        let response: string;
+        try {
+            response = await routeAPINames();
+            res.send(response);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+);
+
+server.get(
+    "/api/weather/:zipcode",
+    function (req: Request, res: Response): void {
+        const response = routeWeather({ zipcode: req.params.zipcode });
+        res.send(response);
+    }
+);
+
+server.get("/components/weather", function (req: Request, res: Response): void {
+    const filePath = path.join(process.cwd(), "public", "weather.html");
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(filePath);
 });
 
-server.listen(port, function () {
+server.listen(port, function (): void {
     console.log('Listening on ' + port);
 });
--- routes.ts
+++ routes.ts
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
-const routeHello = () => "Hello World!";
+const routeHello = (): string => "Hello World!";
 
-const routeAPINames = async () => {
+const routeAPINames = async (): Promise<string> => {
     const url = "https://www.usemodernfullstack.dev/api/v1/users";
-    let data;
+    let data: responseItemType[];
     try {
         const response = await fetch(url);
-        data = await response.json();
+        data = (await response.json()) as responseItemType[];
     } catch (err) {
         return err;
     }
@@ -17,4 +17,14 @@ const routeAPINames = async () => {
     return names;
 };
 
-export { routeHello, routeAPINames };
+const routeWeather = (query: WeatherQueryInterface): WeatherDetailType => queryWeatherData(query);
+
+const queryWeatherData = (query: WeatherQueryInterface): WeatherDetailType => {
+    return {
+        zipcode: query.zipcode,
+        weather: "sunny",
+        temp: 35
+    };
+};
+
+export { routeHello, routeAPINames, routeWeather };
